d55f8d649f8400ec942432f8208a6f86f8584cd9
caoyugang <yugang.cao@tusen.ai>
2023-09-29 13:36:49 +0800

    test: fix error

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,23 @@
 module.exports = {
   roots: ['<rootDir>'],
+  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'vue'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/__tests__/components',
+    '<rootDir>/src/__tests__/directives',
+  ],
+  moduleNameMapper: {
+    '@vue/apollo-composable': [
+      '<rootDir>/node_modules/@vue/apollo-composable/dist/index.js',
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!(@vue/apollo-composable)/)'],
 }
