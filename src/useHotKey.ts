fcceb9198dd06df2dd5ee65c2490f02fdb5708ce
Yugang Cao <yugang.cao12@gmail.com>
2024-04-09 10:16:35 +0800

    feat: update deps

--- src/useHotKey.ts
+++ src/useHotKey.ts
@@ -1,15 +1,15 @@
-import { onUnmounted } from "vue"
-import manager from './hotkeyManager'
-import type { HotKey, RemoveHandler } from "./interface"
+import { onUnmounted } from "vue";
+import manager from "./hotkeyManager";
+import type { HotKey, RemoveHandler } from "./interface";
 
 export default (hotKeys: HotKey[]): RemoveHandler[] => {
-  hotKeys.forEach((hotkey) => manager.registerHotKey(hotkey))
+  hotKeys.forEach((hotkey) => manager.registerHotKey(hotkey));
   onUnmounted(() => {
-    hotKeys.forEach((hk) => manager.removeHotKey(hk))
-  })
+    hotKeys.forEach((hk) => manager.removeHotKey(hk));
+  });
   return hotKeys.map((hk: HotKey) => (customHk?: HotKey) => {
     const deleteHk = customHk ?? hk;
-    manager.removeHotKey(deleteHk)
-  })
-}
-export type { HotKey, RemoveHandler }
+    manager.removeHotKey(deleteHk);
+  });
+};
+export type { HotKey, RemoveHandler };
