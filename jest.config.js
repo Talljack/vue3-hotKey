b74f018243e7f0308ad4e088a2d002fa5d8fbc89
Yugang Cao <yugang.cao12@gmail.com>
2024-05-08 22:28:59 +0800

    workflow: eslint

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   roots: ['<rootDir>'],
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'vue'],
